80d4ad0555be0560e7b777e99267677d9b0817ed
marcoceccarelli98 <mceccarelli98@gmail.com>
2024-07-18 14:51:40 +0200

    [UPD] Project Show Page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,19 +7,21 @@
         {{-- <div class="add-project">
         <a class="btn btn-primary" href="{{ route('projects.create') }}">ADD NEW project</a>
     </div> --}}
-        <div class="projects-list">
-            @foreach ($data['projects'] as $project)
-                <div class="project-card">
-                    <a href="{{ route('projects.show', $project->id) }}">
-                        <div class="thumb-container">
-                            <img src="{{ $project['images'][0] }}" alt="Thumb not found">
-                        </div>
-                        <div class="title-container">
-                            <p>{{ $project['title'] }}</p>
-                        </div>
-                    </a>
-                </div>
-            @endforeach
+        <div class="container">
+            <div class="projects-list">
+                @foreach ($data['projects'] as $project)
+                    <div class="project-card">
+                        <a href="{{ route('projects.show', $project->id) }}">
+                            <div class="image-container">
+                                <img src="{{ $project['images'][0] }}" alt="Thumb not found">
+                            </div>
+                            <div class="title-container">
+                                <p>{{ $project['title'] }}</p>
+                            </div>
+                        </a>
+                    </div>
+                @endforeach
+            </div>
         </div>
     </section>
 @endsection
